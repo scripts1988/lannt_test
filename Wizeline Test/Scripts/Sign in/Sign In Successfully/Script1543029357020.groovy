e594bb6644a566f7d0b819507dfc60e07ae71087
Lan Ngo <lanngo@kms-technology.com>
2018-11-24 13:16:23 +0700

    Delete unused things

--- Wizeline Test/Scripts/Sign in/Sign In Successfully/Script1543029357020.groovy
+++
@@ -1,9 +0,0 @@
-
-
-
-url = 'http://testapp.galenframework.com'
-username = 'testuser@example.com'
-password = 'test123'
-
-"Call a test case to login"
-WebUiBuiltInKeywords.callTestCase(TestCaseFactory.findTestCase('Test Cases/Sign in/Common_Sign In'), ['url' : url, 'username': username, 'password' : password], FailureHandling.STOP_ON_FAILURE)
